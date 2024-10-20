05dafe7821160890c870accac795d009264560e5
Jdure <22172148+Jdure@users.noreply.github.com>
2024-10-19 23:37:03 -0400

    added node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import nodejs from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: nodejs({
+    mode: "standalone",
+  }),
+  output: "server",
   integrations: [react(), tailwind()],
 });
